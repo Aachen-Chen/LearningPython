17f7b5a99ebd26eca6bb07d16199af306698b56a
kaichen <kaichenc@andrew.cmu.edu>
2018-10-14 19:50:47 -0400

    add example on Hands On ML

--- C_ML/SKLearnExample.py
+++ C_ML/SKLearnExample.py
@@ -1,30 +1,32 @@
 from sklearn.datasets import fetch_mldata
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
 mnist = fetch_mldata('MNIST original')
 print(mnist)
 print(type(mnist))
-
 X, y = mnist["data"], mnist["target"]
+
 
 some_digit = X[36000]
 some_digit_image = some_digit.reshape(28, 28)
+import matplotlib
+import matplotlib.pyplot as plt
 plt.imshow(some_digit_image, cmap = matplotlib.cm.binary, interpolation="nearest")
 plt.axis("off")
 plt.show()
-
 print(y[36000])
 
-X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+import numpy as np
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 
 
 y_train_5 = (y_train == 5) # True for all 5s, False for all other digits.
 y_test_5 = (y_test == 5)
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+sgd_clf.predict([some_digit])
 
 
 
